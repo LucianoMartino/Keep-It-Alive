7040056e216638bd81452c95cd9c02edbd592cb5
Luciano Martino <35671003+LucianoMartino@users.noreply.github.com>
2020-04-20 05:01:24 +0100

    Update Bullet.java

--- LD Keep It Alive/Keep Love Alive/src/Bullet.java
+++ LD Keep It Alive/Keep Love Alive/src/Bullet.java
@@ -2,43 +2,51 @@ import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.Rectangle;
 
-public class Bullet extends Projectile{
-	private final int SPEED = 5;
+public class Bullet implements Projectile {
+	
+	private final int SPEED = 2;
 	private final Color BULLET = Color.black;
+	private int x, y;
+	private double xSpeed, ySpeed;
+	private Rectangle bounds;
 	private static int SIZE;
-	public Bullet(int SCALE, int x, int y, double direction) {
+
+	public Bullet(int winScale, int x, int y, double direction) {
 
 		this.x = x;
 		this.y = y;
-		
+
 		xSpeed = Math.cos(direction) * SPEED;
 		ySpeed = Math.sin(direction) * SPEED;
-		
-		SIZE = SCALE * 3;
-		
+
+		SIZE = winScale * 3;
+
 		bounds = new Rectangle(x, y, SIZE, SIZE);
 	}
-	
+
 	@Override
 	public void draw(Graphics2D g) {
 		g.setColor(BULLET);
 		g.fill(bounds);
 	}
+
 	@Override
 	public void tick() {
-		// TODO Auto-generated method stub
-		
+		x += xSpeed;
+		y += ySpeed;
+
+		bounds.setLocation(x, y);
 	}
+
 	@Override
 	public void destroy() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
-	public static int getSize() {
-		return SIZE;
+	@Override
+	public Rectangle getBounds() {
+		// TODO Auto-generated method stub
+		return null;
 	}
-
-	
-	
 }
