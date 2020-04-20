8963c2df2bf11b2cfbd8f35efa0a98c8af0e0903
Luciano Martino <35671003+LucianoMartino@users.noreply.github.com>
2020-04-20 05:00:22 +0100

    Update Projectile.java

--- LD Keep It Alive/Keep Love Alive/src/Projectile.java
+++ LD Keep It Alive/Keep Love Alive/src/Projectile.java
@@ -1,15 +1,12 @@
 import java.awt.Graphics2D;
 import java.awt.Rectangle;
 
-public abstract class Projectile {
-	protected Rectangle bounds;
-	protected int x, y;
-	protected double xSpeed, ySpeed;
-	public abstract void draw(Graphics2D g);
-	public abstract void tick();
-	public abstract void destroy();
-	
-	public Rectangle getBounds() {
-		return bounds;
-	}
+public interface Projectile {
+	void draw(Graphics2D g);
+
+	void tick();
+
+	void destroy();
+
+	Rectangle getBounds();
 }
